f442cb8a0636cec57215c824c1072091ce897341
sanchesleo <sanchesleoalves@gmail.com>
2021-06-28 19:43:51 -0300

    Projeto em React com template em typescript

---
+++ src/components/button.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+type ButtonProps = {
+    text: string;
+}
+
+export function Button(props: ButtonProps) {
+    const [counter, setCounter] = useState(0);
+    return(
+        <>
+            <button onClick={() => setCounter(counter+1)}>          {props.text}
+            </button>
+            <p>{counter}</p>
+        </>
+    )
+}
